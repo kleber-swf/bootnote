0f4455ca8021824dc16428dc0a9aeb233a1dc70a
Kleber Silva <kleber.swf@gmail.com>
2016-06-02 15:51:12 -0300

    * simplified type name (better to remember): info/normal/(empty)/(invalid), warning/warn, error/danger/critical/important, education/tip, web, question, bug and quote * simplified the html generated by the shortcode * beautiful colors * using Genericons font (still depends on theme)

--- syntax.php
+++ syntax.php
@@ -9,133 +9,91 @@ if (!defined('DOKU_INC')) die();
 
 class syntax_plugin_bootnote extends DokuWiki_Syntax_Plugin {
 
-    public function getType() {
-        return 'container';
-    }
+	public function getType() { return 'container'; }
 
-    /**
-    * @return string Paragraph type
-    **/
+	/**
+	* @return string Paragraph type
+	**/
+	public function getPType() { return 'normal'; }
 
-    public function getPType() {
-        return 'normal';
-    }
+	// Keep syntax inside plugin
+	function getAllowedTypes() {
+		return array('container', 'baseonly', 'substition','protected','disabled','formatting','paragraphs');
+	}
 
-    // Keep syntax inside plugin
-    function getAllowedTypes() {
-	return array('container', 'baseonly', 'substition','protected','disabled','formatting','paragraphs');
-    }
+	public function getSort() { return 192; }
 
-    public function getSort() {
-        return 192;
-    }
+	function connectTo($mode) {
+		if ( $this->getConf( 'bootnote.note' ) == 'note' )
+			$this->Lexer->addEntryPattern( '<note[^>]*>(?=.*</note>)', $mode, 'plugin_bootnote' );
+		else
+			$this->Lexer->addEntryPattern( '<bootnote[^>]*>(?=.*</bootnote>)', $mode, 'plugin_bootnote' );
+	}
+	
+	function postConnect() {
+		if ( $this->getConf('bootnote.note') == 'note' )
+			$this->Lexer->addExitPattern( "</note>", 'plugin_bootnote' );
+		else
+			$this->Lexer->addExitPattern( "</bootnote>", 'plugin_bootnote' );
+	}
 
-    function connectTo($mode) {
-        if($this->getConf('bootnote.note') == 'note') {
-            $this->Lexer->addEntryPattern('<note[^>]*>(?=.*</note>)', $mode,'plugin_bootnote');
-        } else {
-            $this->Lexer->addEntryPattern('<bootnote[^>]*>(?=.*</bootnote>)', $mode,'plugin_bootnote');
-        }
-    }
-    
-    function postConnect() {
-        if($this->getConf('bootnote.note') == 'note') {
-            $this->Lexer->addExitPattern("</note>", 'plugin_bootnote');
-        } else {
-            $this->Lexer->addExitPattern("</bootnote>", 'plugin_bootnote');
-        }
-    }
+	/**
+	* Do the regexp
+	**/
+	function handle( $match, $state, $pos, Doku_Handler $handler ) {
+		switch( $state ){
+			case DOKU_LEXER_ENTER :
+				$data = array(
+					'state'	=> $state,
+					'lvl'	=> ''
+				);
+				// Looking for id
+				$note = $this->getConf( 'bootnote.note' ) == 'note' ? 'note' : 'bootnote';
+				preg_match( "/$note (\\w*)/", $match, $lvl );
+				if ( count( $lvl ) != 0 ) $data['lvl'] = $lvl[1];
+				return $data;           
+			case DOKU_LEXER_UNMATCHED :
+				return array( 'state'=>$state, 'text'=>$match );
+			default:
+				return array( 'state'=>$state, 'bytepos_end' => $pos + strlen( $match ) );
+		 }
+	}
 
-/**
-* Do the regexp
-**/
-    function handle($match, $state, $pos, Doku_Handler $handler) {
-        switch($state){
-            case DOKU_LEXER_ENTER :
-                $data = array(
-                        'state'=>$state,
-                        'lvl'=> "",
-                    );
-                // Looking for id
-                if($this->getConf('bootnote.note') == 'note') {
-                    $note = 'note';
-                } else {
-                    $note = 'bootnote';
-                }
-                preg_match("/$note (\\w*)/", $match, $lvl);
-                if( count($lvl) != 0 ) {
-                    $data['lvl'] = $lvl[1];
-                }
-                return $data;           
-            case DOKU_LEXER_UNMATCHED :
-                return array('state'=>$state, 'text'=>$match);
-            default:
-                return array('state'=>$state, 'bytepos_end' => $pos + strlen($match));
-         }
-    }
+	function _render_note($renderer, $data) {
+		 $renderer->doc .= '<div class="note-container n-' . $data['lvl'] . '">';
+		 $renderer->doc .= '<div class="note">';
+	}
 
-    function _render_note($renderer, $data, $glyph) {
-         $renderer->doc .= '<div>'; // Global
-         $renderer->doc .= '<div class="sign-container">';
-         $renderer->doc .= '  <span class="sign">';
-         $renderer->doc .= '    <span class="glyphicon glyphicon-'.$glyph.' s-'.$glyph.' glyph" aria-hidden="true"></span>';
-         $renderer->doc .= '  </span>'; // /.sign
-         $renderer->doc .= '</div>'; // /.sign-container
-         $renderer->doc .= '<div class="note-container s-'.$glyph.'">';
-         $renderer->doc .= '<div class="note">';
-    }
+	/****
+	* MAIN FUNCTION
+	****/
+	function _define_note($renderer, $data) {
+		if ( !$data['lvl'] ) $data['lvl'] = 'info';
+		$this->_render_note( $renderer, $data );
+	}
 
-    /****
-    * MAIN FONCTION
-    ****/
-    function _define_note($renderer, $data) {
-	    if($data['lvl'] == "web") {
-            $glyph = "globe";
-            $this->_render_note($renderer, $data, $glyph);
-	    }elseif($data['lvl'] == "question") {
-            $glyph = "question-sign";
-            $this->_render_note($renderer, $data, $glyph);
-        }elseif($data['lvl'] == "learn" || $data['lvl'] == "tip") {
-            $glyph = "education";
-            $this->_render_note($renderer, $data, $glyph);
-         }elseif($data['lvl'] == "warning") {
-            $glyph = "alert";
-            $this->_render_note($renderer, $data, $glyph);
-        }elseif($data['lvl'] == "critical" || $data['lvl'] == "important") {
-            $glyph = "fire";
-            $this->_render_note($renderer, $data, $glyph);
-	    }else{
-            $glyph = "info-sign";
-            $this->_render_note($renderer, $data, $glyph);
-        }
-    }
+	// Dokuwiki Renderer
+	function render( $mode, Doku_Renderer $renderer, $data ) {
+		if ( $mode != 'xhtml' ) return false;
 
-    // Dokuwiki Renderer
-    function render($mode, Doku_Renderer $renderer, $data) {
-        if($mode != 'xhtml') return false;
+		if ( $data['error'] ) {
+			$renderer->doc .= $data['text'];
+			return true;
+		}
+		$renderer->info['cache'] = false;
+		switch ( $data['state'] ) {
+			case DOKU_LEXER_ENTER :
+				$this->_define_note( $renderer, $data );
+				break;
+			case DOKU_LEXER_EXIT:
+				$renderer->doc .= '</div>';// /.note
+				$renderer->doc .= '<div class="triangle"></div>';
+				$renderer->doc .= '</div>';// /.note-container
 
-        if($data['error']) {
-            $renderer->doc .= $data['text'];
-            return true;
-        }
-        $renderer->info['cache'] = false;
-        switch($data['state']) {
-            case DOKU_LEXER_ENTER :
-                $this->_define_note($renderer, $data);
-                break;
-            case DOKU_LEXER_EXIT:
-                $renderer->doc .= '</div>';// /.note
-                $renderer->doc .= '<div class="triangle"></div>';
-                $renderer->doc .= '</div>';// /.note-container
-                $renderer->doc .= '</div>';// /Global
-
-            case DOKU_LEXER_UNMATCHED :
-                $renderer->doc .= $renderer->_xmlEntities($data['text']);
-                break;
-        }
-        return true;
-    }
+			case DOKU_LEXER_UNMATCHED :
+				$renderer->doc .= $renderer->_xmlEntities( $data['text'] );
+				break;
+		}
+		return true;
+	}
 }
-
-
-
